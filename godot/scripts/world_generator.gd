095b05a20b4f1d3f032496163c889b1090b81b21
NestiaDev-id <yohanesdevano90@gmail.com>
2026-04-18 23:54:38 +0700

    feat: setup frontend

---
+++ godot/scripts/world_generator.gd
@@ -0,0 +1,12 @@
+extends WorldGenerator
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	spawn_random_cubes(50)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
